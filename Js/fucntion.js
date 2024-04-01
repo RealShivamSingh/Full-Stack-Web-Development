8dec9d444ec308ac79332e3b021d1080e1f0e970
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2024-04-01 15:45:05 +0530

    Create fucntion.js

---
+++ Js/fucntion.js
@@ -0,0 +1,22 @@
+function number(){
+    for(let i=0;i<5;i++){
+        console.log(i);
+    }
+}
+number();
+
+//Age checker
+
+function eligibleAge(){
+    let age=75;
+    if(age<=20 && age>=50){
+        console.log("Javan Munda");
+    }
+    else if(age<=50){
+        console.log("Buda hai tu");
+    }
+    else{
+        console.log("Bcha hai abhi tu");
+    }
+}
+eligibleAge();
