3c7e5a469cc08a73f1a73993d2e41e306ceb05cc
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2024-04-01 15:45:01 +0530

    Create forOfLoop.js

---
+++ Js/forOfLoop.js
@@ -0,0 +1,10 @@
+// let fruits=["Mango","Orange","Guvava","Lichi"];
+// for(fruit of fruits){
+//     console.log(fruit);
+// }
+let heroes=[["IronMan","CaptainAmerica","Flash","Adam"],["Hulk","Vision","Thor"]];
+for(list of heroes){
+    for(hero of heroes){
+    console.log(hero);
+}
+}
