960adfccfdbe27e79eaad6e0132b4a2eb92f500d
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2024-04-02 14:01:38 +0530

    Function Return

---
+++ Js/fuctionReturn.js
@@ -0,0 +1,15 @@
+ function addtion(a,b){
+    return a+b;
+ }
+ console.log(addtion(5,9));
+
+ //Another function return 
+  function ageCheck(n){
+    if(n>18){
+        return "Mature Ladka";
+    }
+    else{
+        return "Mature nhi hai";
+    }
+  }
+  console.log(ageCheck(5));
