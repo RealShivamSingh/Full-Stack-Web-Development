f35902f7243164ea184d4658cd8d4ba20934df3f
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2024-04-05 20:34:27 +0530

    try and catch

---
+++ Js/tryCatch.js
@@ -0,0 +1,11 @@
+//console.log(a);
+//on the avobe code of block it will through a arror because a is not define 
+//that's we use try and catch to find error in code of block
+
+try{
+    console.log(a);
+}
+catch(error){
+    console.log(error);
+
+}
