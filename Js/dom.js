982edb0aefb799c66a9853abe47631dc76650b08
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2024-04-21 22:13:47 +0530

    DOM

---
+++ Js/dom.js
@@ -0,0 +1,5 @@
+console.log("Shivaaaa")
+document.body.firstElementChild
+document.body.firstElementChild.children
+document.body.firstElementChild.children[4]
+document.body.firstElementChild.nextElementSibling;
