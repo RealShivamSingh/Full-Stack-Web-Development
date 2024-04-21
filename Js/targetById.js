c0ccbc936316826918c31ebb87706fe81b861e35
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2024-04-21 22:14:13 +0530

    working with dom or targeting classes and id by using DOM

---
+++ Js/targetById.js
@@ -0,0 +1,7 @@
+console.log("Shivii")
+let shivi=document.getElementsByClassName("box");
+console.log(shivi);
+shivi[2].style.backgroundColor="red"
+
+//Targeting on a particular box 
+document.getElementById("greenBox").style.backgroundColor="green"
